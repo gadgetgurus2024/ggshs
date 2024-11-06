43763e8c938693366cc30fbf19f96c63023cbe90
gadgetgurus2024 <gadgetgurus497@gmail.com>
2024-11-06 20:05:53 +0100

    Add files via upload

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,27 @@
 
 let folder='images/gallery/'
 
-for(let i=1; i<10;i++){
+for(let i=1; i<27;i++){
     
-    let flexContainer=document.createElement('div')
-    let imageContainer=document.createElement('div')
+    //let flexContainer=document.createElement('div')
+    //let imageContainer=document.createElement('div')
     let photo=document.createElement('img')
     photo.setAttribute('src',folder+i+'.jpg')
+    photo.style.margin='10px'
 
-    imageContainer.appendChild(photo)
-    flexContainer.appendChild(imageContainer)
-    flexContainer.classList.add('photos')
-    //document.body.appendChild(flexContainer)
+    //imageContainer.appendChild(photo)
+    //flexContainer.appendChild(imageContainer)
+   
+    document.body.appendChild(photo)
+    //flexContainer.classList.add('memories')
+    
+    //console.log(folder+i);
+    
 }
+
+let loading=document.createElement('h2')
+loading.innerHTML='This is just the tip of the iceberg. <br>More to come soon.'
+document.body.appendChild(loading)
 
 
 
